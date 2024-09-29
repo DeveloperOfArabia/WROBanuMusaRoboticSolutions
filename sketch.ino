b38123ea68aa72a587f634cd23d73b411ea58e48
DeveloperOfArabia <147246109+DeveloperOfArabia@users.noreply.github.com>
2024-09-29 18:04:44 +0300

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,15 +1,26 @@
-int sensorPin = A0;
+int soilMoisturePin = A0;
+int ldrPin = A1;
+int ntcPin = A2;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin);
+  int soilMoistureValue = analogRead(soilMoisturePin);
+  int ldrValue = analogRead(ldrPin);
+  int ntcValue = analogRead(ntcPin);
+
+  // Depending on temperature sensor used, equation might change.
+  float voltage = ntcValue * (5.0 / 1023.0);
+  float temperatureC = (voltage - 0.5) * 100.0;
 
   Serial.print("Soil Moisture: ");
-  Serial.print(sensorValue);
-  Serial.println();
+  Serial.print(soilMoistureValue);
+  Serial.print(", LDR: ");
+  Serial.print(ldrValue);
+  Serial.print(", Temperature (C): ");
+  Serial.println(temperatureC);
 
   delay(1000);
 }
